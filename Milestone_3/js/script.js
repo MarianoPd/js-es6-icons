5e6bc04a7489255d3a1707f8ed8ad301b490d551
MarianoPd <marianopidone.com>
2021-11-08 17:50:54 +0100

    Milestone 3

---
+++ Milestone_3/js/script.js
@@ -0,0 +1,149 @@
+const data = [
+	{
+		name: 'cat',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'crow',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dog',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dove',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dragon',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'horse',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'hippo',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'fish',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'carrot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'apple-alt',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'lemon',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'pepper-hot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'user-astronaut',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-graduate',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-ninja',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-secret',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	}
+];
+
+
+const wrap = document.getElementById('main-wrap');
+
+layCards(data, wrap);
+
+const select = document.getElementById('type');
+
+select.addEventListener('change', (event) =>{
+	
+	let specificType = data.filter((element) => {
+		if(element.type === event.target.value || event.target.value === "all"){
+			return true;
+		}else return false;
+	});
+	
+	layCards(specificType, wrap);
+});
+
+
+
+function layCards(array, container){
+	container.innerHTML = '';
+    array.forEach(element => {
+
+        const newCard = document.createElement("div");
+        newCard.className = "card";
+        container.appendChild(newCard);
+        let text = `
+            <i class="${element.family} ${element.prefix}${element.name}" style="color: ${element.color}"></i>
+            <span>${element.name}</span>
+        `;
+        newCard.innerHTML += text;
+    });
+}
